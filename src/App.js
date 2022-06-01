3f88d6fca3f38ea6f265654ebfbb59ad4ce7b9e1
paul.mandl <paul.mandl@findustrial.io>
2022-06-01 17:59:04 +0200

    button add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <a
+          href="https://twitter.com/moonblurgs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button id="home_button"></button>
+        </a>
+      </header>
     </div>
   );
 }
