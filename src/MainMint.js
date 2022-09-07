21ec3b6638836afe2c5443a231bf5e57fa0ae6d2
paul.mandl <paul.mandl@findustrial.io>
2022-09-07 14:39:03 +0200

    hypercard

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { ethers, BigNumber } from "ethers";
 import { Box, Flex, Text, Button, Input } from "@chakra-ui/react";
-import moonblurgs from "./moonblurgs.json";
+import Hypercard from "./hypercard.json";
 
-const moonblurgsAddress = "0x5e550f60Bc59a225540d40D550860c11ec552902";
+const moonblurgsAddress = "0xBa28D20DE0e9b91c06880A59aB76fCd320690049";
 
 export const MainMint = ({ accounts, setAccounts }) => {
   const [mintAmount, setMintAmount] = useState(1);
@@ -17,14 +19,14 @@ export const MainMint = ({ accounts, setAccounts }) => {
       const signer = provider.getSigner();
       const contract = new ethers.Contract(
         moonblurgsAddress,
-        moonblurgs.abi,
+        Hypercard.abi,
         signer
       );
       try {
         const response = await contract.totalSupply();
         setTotalSupply(response.toNumber());
       } catch (e) {
-        console.log("error: ", e);
+        toast(e.error.message);
       }
     }
   }
@@ -34,7 +36,7 @@ export const MainMint = ({ accounts, setAccounts }) => {
       const signer = provider.getSigner();
       const contract = new ethers.Contract(
         moonblurgsAddress,
-        moonblurgs.abi,
+        Hypercard.abi,
         signer
       );
       try {
@@ -44,7 +46,7 @@ export const MainMint = ({ accounts, setAccounts }) => {
         );
         console.log("response: ", response);
       } catch (e) {
-        console.log("error: ", e);
+        toast(e.error.message);
       }
     }
   }
@@ -58,110 +60,116 @@ export const MainMint = ({ accounts, setAccounts }) => {
   };
   getTotalSupply();
   return (
-    <Flex justify="center" align="center" height="100vh" marginBottom="250px">
-      <Box width="520px">
-        <div>
-          <Text fontSize="35px" textShadow="0 5px #0F0F0F" fontFamily="inherit">
-            HyperCards by Hypercard
-          </Text>
-          <Text
-            fontSize="20px"
-            textShadow="0 2px 2px #0F0F0F"
-            fontFamily="VT323"
-          >
-            A collection of 999 HyperCards on the Ethereum Blockchain. Holding a
-            HyperCard will grant you access to pre-sale and whitelist spots for
-            all our upcoming projects. Depending on the supply of the project,
-            everyone will get one or it will be first come first serve.{" "}
-          </Text>
-        </div>
-        {isConnected ? (
-          isSoldOut ? (
-            <div style={{ marginBottom: "100px" }}>
-              <Flex justify="space-evenly" align="center">
-                <Text
-                  fontSize="30px"
-                  textShadow="0 5px #0F0F0F"
-                  fontFamily="VT323"
-                >
-                  0 / 999 HyperCards Minted
-                </Text>
-              </Flex>
-              <Flex justify="space-evenly" align="center">
-                <Button
-                  onClick={handleDecrement}
-                  borderRadius="5px"
-                  backgroundColor="#55AEEF"
-                  boxShadow="0px 2px 2px 1px #0F0F0F"
-                  fontFamily="inherit"
-                  color="white"
-                  cursor="pointer"
-                  margin="0 15px"
-                >
-                  -
-                </Button>
-                <Input
-                  readOnly
-                  fontFamily="inherit"
-                  width="40px"
-                  height="30px"
-                  textAlign="center"
-                  paddingLeft="19px"
-                  marginTop="10px"
-                  type="number"
-                  value={mintAmount}
-                />
-                <Button
-                  onClick={handleIncrement}
-                  borderRadius="5px"
-                  backgroundColor="#55AEEF"
-                  boxShadow="0px 2px 2px 1px #0F0F0F"
-                  fontFamily="inherit"
-                  color="white"
-                  cursor="pointer"
-                  margin="0 15px"
-                >
-                  +
-                </Button>
-              </Flex>
-              <Flex justify="space-evenly" align="center">
-                <Button
-                  onClick={handleMint}
-                  borderRadius="5px"
-                  backgroundColor="#55AEEF"
-                  boxShadow="0px 2px 2px 1px #0F0F0F"
-                  fontFamily="inherit"
-                  color="white"
-                  height="30px"
-                  cursor="pointer"
-                  marginTop="15px"
-                >
-                  Mint
-                </Button>
-              </Flex>
-            </div>
-          ) : (
+    <div>
+      <Flex justify="center" align="center" height="100vh" marginBottom="250px">
+        <Box width="520px">
+          <div>
             <Text
-              fontSize="30px"
-              textShadow="0 2px 2px #0F0F0F"
-              fontFamily="VT323"
+              fontSize="35px"
+              textShadow="0 5px #0F0F0F"
+              fontFamily="inherit"
             >
-              HyperCards are all minted out. Grab one on Opensea!
+              HyperCards by Hypercard
             </Text>
-          )
-        ) : (
-          <div style={{ marginBottom: "100px" }}>
             <Text
-              fontSize="30px"
+              fontSize="20px"
               textShadow="0 2px 2px #0F0F0F"
               fontFamily="VT323"
             >
-              You need to connect your wallet, to mint a HyperCard.
+              A collection of 999 HyperCards on the Ethereum Blockchain. Holding
+              a HyperCard will grant you access to pre-sale and whitelist spots
+              for all our upcoming projects.
             </Text>
           </div>
-        )}
-        <div class="attribution-block" style={{ fontSize: "5px" }}></div>{" "}
-      </Box>
-    </Flex>
+          {isConnected ? (
+            isSoldOut ? (
+              <div style={{ marginBottom: "100px" }}>
+                <Flex justify="space-evenly" align="center">
+                  <Text
+                    fontSize="30px"
+                    textShadow="0 5px #0F0F0F"
+                    fontFamily="VT323"
+                  >
+                    {totalSupply} / 999 HyperCards Minted
+                  </Text>
+                </Flex>
+                <Flex justify="space-evenly" align="center">
+                  <Button
+                    onClick={handleDecrement}
+                    borderRadius="5px"
+                    backgroundColor="#55AEEF"
+                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                    fontFamily="inherit"
+                    color="white"
+                    cursor="pointer"
+                    margin="0 15px"
+                  >
+                    -
+                  </Button>
+                  <Input
+                    readOnly
+                    fontFamily="inherit"
+                    width="40px"
+                    height="30px"
+                    textAlign="center"
+                    paddingLeft="19px"
+                    marginTop="10px"
+                    type="number"
+                    value={mintAmount}
+                  />
+                  <Button
+                    onClick={handleIncrement}
+                    borderRadius="5px"
+                    backgroundColor="#55AEEF"
+                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                    fontFamily="inherit"
+                    color="white"
+                    cursor="pointer"
+                    margin="0 15px"
+                  >
+                    +
+                  </Button>
+                </Flex>
+                <Flex justify="space-evenly" align="center">
+                  <Button
+                    onClick={handleMint}
+                    borderRadius="5px"
+                    backgroundColor="#55AEEF"
+                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                    fontFamily="inherit"
+                    color="white"
+                    height="30px"
+                    cursor="pointer"
+                    marginTop="15px"
+                  >
+                    Mint
+                  </Button>
+                </Flex>
+              </div>
+            ) : (
+              <Text
+                fontSize="30px"
+                textShadow="0 2px 2px #0F0F0F"
+                fontFamily="VT323"
+              >
+                HyperCards are all minted out. Grab one on Opensea!
+              </Text>
+            )
+          ) : (
+            <div style={{ marginBottom: "100px" }}>
+              <Text
+                fontSize="30px"
+                textShadow="0 2px 2px #0F0F0F"
+                fontFamily="VT323"
+              >
+                You need to connect your wallet, to mint a HyperCard.
+              </Text>
+            </div>
+          )}
+          <div className="attribution-block" style={{ fontSize: "5px" }}></div>{" "}
+        </Box>
+      </Flex>
+      <ToastContainer />
+    </div>
   );
 };
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { ethers, BigNumber } from "ethers";
 import url from "./assets/music/sound.mp3";
 import { Box, Flex, Spacer, Button } from "@chakra-ui/react";
 import logo from "./assets/background/hyper.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const NavBar = ({ accounts, setAccounts }) => {
   const useAudio = (url) => {
@@ -29,88 +32,114 @@ export const NavBar = ({ accounts, setAccounts }) => {
 
   async function connectAccount() {
     if (window.ethereum) {
+      toggle();
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-      if (accounts) setAccounts(accounts);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const network = await provider.getNetwork();
+      if (accounts && network.chainId === 4) setAccounts(accounts);
+      else {
+        toast(
+          "Wrong network selected! You need to be connected to ethereum mainnet in order to mint."
+        );
+      }
     }
   }
   function disconnect() {
     setAccounts([]);
   }
   return (
-    <Flex justify="space-between" align="center" padding="30px">
-      <Flex justify="space-around" width="30%" padding="10px 70px">
-        <img src={logo} height="100px" style={{ marginTop: "-30px" }}></img>
-        <div
-          style={{
-            marginRight: "10px",
-            marginTop: "30px",
-            cursor: "pointer",
-            fontSize: "11px",
-          }}
-        >
-          <a
-            href="https://twitter.com/hypercardhero"
-            target="_blank"
-            style={{ textDecoration: "none", color: "white" }}
+    <div>
+      <Flex justify="space-between" align="center" padding="30px">
+        <Flex justify="space-around" width="30%" padding="10px 70px">
+          <img
+            src={logo}
+            height="100px"
+            className="logo"
+            style={{ marginTop: "-30px" }}
+          ></img>
+          <div
+            style={{
+              marginRight: "10px",
+              marginTop: "30px",
+              cursor: "pointer",
+              fontSize: "11px",
+              className: "text",
+            }}
           >
-            Twitter
-          </a>
-        </div>
-        <div
-          style={{ marginLeft: "10px", marginTop: "30px", fontSize: "11px" }}
-        >
-          Opensea
-        </div>
-      </Flex>
-      <Flex justify="space-around" width="20%">
-        <Button
-          onClick={toggle}
-          borderRadius="5px"
-          backgroundColor="#55AEEF"
-          boxShadow="0px 2px 2px 1px #0F0F0F"
-          fontFamily="inherit"
-          color="white"
-          height="30px"
-          fontSize="11px"
-          cursor="pointer"
-          marginLeft="15px"
-          marginTop="5px"
-          className="onlyDesktop"
-        >
-          {playing ? "Sound off" : "Sound on"}
-        </Button>
-        <div className="marginOnPhone">
-          {isConnected ? (
-            <Box
-              marginLeft="15px"
-              marginTop="15px"
-              cursor="pointer"
-              fontSize="11px"
-              onClick={disconnect}
+            <a
+              href="https://twitter.com/hypercardhero"
+              target="_blank"
+              style={{
+                textDecoration: "none",
+                color: "white",
+              }}
             >
-              Connected
-            </Box>
-          ) : (
-            <Button
-              onClick={connectAccount}
-              borderRadius="5px"
-              backgroundColor="#55AEEF"
-              boxShadow="0px 2px 2px 1px #0F0F0F"
-              fontFamily="inherit"
-              color="white"
-              fontSize="11px"
-              height="30px"
-              cursor="pointer"
-              marginLeft="15px"
-              marginTop="5px"
-            >
-              Connect
-            </Button>
-          )}
-        </div>
+              Twitter
+            </a>
+          </div>
+          <div
+            className="onlyDesktop"
+            style={{
+              marginLeft: "10px",
+              marginTop: "30px",
+              fontSize: "11px",
+              className: "text",
+            }}
+          >
+            Opensea
+          </div>
+        </Flex>
+        <Flex justify="space-around" width="20%">
+          <Button
+            onClick={toggle}
+            borderRadius="5px"
+            backgroundColor="#55AEEF"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            fontFamily="inherit"
+            color="white"
+            height="30px"
+            fontSize="11px"
+            cursor="pointer"
+            marginLeft="15px"
+            marginTop="5px"
+            className="onlyDesktop"
+          >
+            {playing ? "Sound off" : "Sound on"}
+          </Button>
+          <div className="marginOnPhone">
+            {isConnected ? (
+              <Box
+                marginLeft="15px"
+                marginTop="15px"
+                cursor="pointer"
+                fontSize="11px"
+                onClick={disconnect}
+              >
+                Connected
+              </Box>
+            ) : (
+              <Button
+                onClick={connectAccount}
+                borderRadius="5px"
+                backgroundColor="#55AEEF"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                fontFamily="inherit"
+                color="white"
+                fontSize="11px"
+                height="30px"
+                cursor="pointer"
+                marginLeft="15px"
+                marginTop="5px"
+              >
+                Connect
+              </Button>
+            )}
+          </div>
+        </Flex>
       </Flex>
-    </Flex>
+      <ToastContainer />
+    </div>
   );
 };
